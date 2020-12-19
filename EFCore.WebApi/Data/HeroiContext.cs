dea6150dd3235890613ebf21453366589d75fe43
Rodrigo <rodrigo.muniz.dev@gmail.com=>
2020-12-19 03:15:50 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore.WebApi/Data/HeroiContext.cs
@@ -0,0 +1,30 @@
+﻿using EFCore.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebApi.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<IdentidadeSecreta> identidadeSecretas { get; set; }
+        public DbSet<HeroiBatalha> HeroiBatalhas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HeroApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity=>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
---
+++ EFCore.WebApi/Models/IdentidadeSecreta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebApi.Models
+{
+    public class IdentidadeSecreta
+    {
+        public int Id { get; set; }
+        public string Nomereal { get; set; }
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+    }
+}
